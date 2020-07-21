d727f00403c4fba2ee8edebccf3cf69b6be2ea9d
Joel Oguntoye <joeloguntoye@gmail.com>
2020-07-21 13:51:01 +0100

    Refactor:Using dagger hilt modules to provide locationClient, pending intent and notification builder

--- app/src/main/java/com/example/runningtracker/services/Trackingservice.kt
+++ app/src/main/java/com/example/runningtracker/services/Trackingservice.kt
@@ -5,6 +5,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_LOW
 import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
 import android.location.Location
@@ -12,6 +13,7 @@ import android.os.Build
 import android.os.Looper
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
+import androidx.core.os.persistableBundleOf
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
@@ -34,20 +36,30 @@ import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.maps.model.LatLng
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import javax.inject.Inject
 
-    typealias  polyLine = MutableList<LatLng>
+typealias  polyLine = MutableList<LatLng>
     typealias  polyLines = MutableList<polyLine>
 
+    @AndroidEntryPoint
     class TrackingService: LifecycleService() {
+
 
         var isFirstRun = true
 
+        @Inject
         lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+        @Inject
+        lateinit var  baseNotificationBuilder :NotificationCompat.Builder
+
+        lateinit var curNotificationBuilder : NotificationCompat.Builder
 
         private val timeRunInSeconds = MutableLiveData<Long>()
 
@@ -67,11 +79,13 @@ import timber.log.Timber
 
         override fun onCreate() {
             super.onCreate()
+            curNotificationBuilder = baseNotificationBuilder
             postInitialValues()
             fusedLocationProviderClient = FusedLocationProviderClient(this)
 
             isTracking.observe(this, Observer {
                 updateLocationTracking(it)
+                updateNotificationTrackingState(it)
             })
         }
 
@@ -135,6 +149,37 @@ import timber.log.Timber
             isTracking.postValue(false)
             isTimerEnabled = false
         }
+
+        private fun updateNotificationTrackingState(isTracking: Boolean) {
+            val notificationActionText = if(isTracking) "Pause" else "Resume"
+            val pendingIntent = if(isTracking) {
+                val pauseIntent =    Intent(this, TrackingService::class.java).apply {
+                    action = ACTION_PAUSE_SERVICE
+                }
+                PendingIntent.getService(this, 1, pauseIntent, FLAG_UPDATE_CURRENT)
+            }
+            else { val resumeIntent = Intent(this, TrackingService::class.java).apply {
+                action = ACTION_START_OR_RESUME_SERVICE
+            }
+                PendingIntent.getService(this, 2, resumeIntent, FLAG_UPDATE_CURRENT)
+
+            }
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            curNotificationBuilder.javaClass.getDeclaredField("mActions").apply {
+                isAccessible = true
+                set(curNotificationBuilder,ArrayList<NotificationCompat.Action>())
+            }
+
+            curNotificationBuilder = baseNotificationBuilder
+                .addAction(R.drawable.ic_pause_black_24dp,notificationActionText, pendingIntent)
+            notificationManager.notify(NOTIFICATION_ID, curNotificationBuilder.build())
+        }
+
+
+
+
 
         @SuppressLint("MissingPermission")
         private fun updateLocationTracking(isTracking: Boolean) {
@@ -203,29 +248,18 @@ import timber.log.Timber
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             createNotificationChannel(notificationManager)
         }
-        val  notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
-            .setAutoCancel(false)
-            .setOngoing(true)
-            .setSmallIcon(R.drawable.ic_directions_run_black_24dp)
-            .setContentTitle("Running App")
-            .setContentText("00:00:00")
-            .setContentIntent(getMainActivityPendingIntent())
 
-        startForeground(NOTIFICATION_ID, notificationBuilder.build())
 
+        startForeground(NOTIFICATION_ID, baseNotificationBuilder.build())
+
+
+        timeRunInSeconds.observe(this, Observer {
+            val notification = curNotificationBuilder
+                .setContentText(TrackingUtils.getFormattedStopWatchTime(it * 1000L))
+            notificationManager.notify(NOTIFICATION_ID, notification.build())
+        })
 
     }
-
-    private  fun getMainActivityPendingIntent() = PendingIntent.getActivity(
-        this,
-        0,
-        Intent(this, MainActivity::class.java).also {
-            it.action = ACTION_SHOW_TRACKING_FRAGMENT
-        },
-        PendingIntent.FLAG_UPDATE_CURRENT
-    )
-
-
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun createNotificationChannel(notificationManager: NotificationManager) {
